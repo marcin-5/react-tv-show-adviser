a9a02ebb76a9d7148553b461a855fb11120ffe2e
Marcin Bojara <marcin_5@mail.ru>
2023-10-24 21:19:38 +0200

    cleaning the input after a request

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,31 @@
-import s from "./style.module.css";
+import { useState } from "react";
 import { Search as SearchIcon } from "react-bootstrap-icons";
+import s from "./style.module.css";
 
-export function SearchBar({onSubmit}) {
+export function SearchBar({ onSubmit }) {
+  const [value, setValue] = useState("");
+
   function submit(e) {
-    if (e.key === "Enter" && e.target.value.trim() !== "") onSubmit(e.target.value);
+    if (e.key === "Enter" && e.target.value.trim() !== "") {
+      onSubmit(e.target.value);
+      setValue("");
+    }
+  }
+
+  function handleChange(e) {
+    setValue(e.target.value);
   }
   return (
     <>
       <SearchIcon size={27} className={s.icon} />
-      <input className={s.input} onKeyUp={submit} type="text" placeholder="Search a tv show you may like" />
+      <input
+        className={s.input}
+        onKeyUp={submit}
+        onChange={handleChange}
+        type="text"
+        value={value}
+        placeholder="Search a tv show you may like"
+      />
     </>
   );
 }
